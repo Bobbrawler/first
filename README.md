fb5f8196c4f9babb3889703163e7fa350240c88e
bobik_on_computer <bobik.gmail.com>
2024-09-01 14:00:07 +0300

    redacting

--- README.md
+++ README.md
@@ -1,25 +1,48 @@
-# Site Car Selection
-Site Car Selection is a web application that allows users to select a car from a drop-down list. When a car is selected, the corresponding image is displayed.
+# Site Item Selection
+Site Item Selection is a web application that allows users to select a item from a drop-down list. When a item is selected, the corresponding image is displayed.
 
 ## Installation and Run
 Clone the repository:
 ```
-git clone https://github.com/Bobbrawler/site-car-selection.git
+git clone https://github.com/Bobbrawler/site-item-selection.git
 ```
 Go to the project folder:
 ```
-cd choose-car
+cd choose-item
 ```
 Open index.html in a web browser: Find the index.html file in the project folder and open it in any modern web browser (Chrome, Firefox, Safari, etc.).
 
+To enter your database for selection, you need to create a JSON file ``` 
+meta.json
+```
+and place it along the path
+```
+resources/data
+```
+An example of such a file:
+```
+[
+    {
+      "name": "name1",
+      "imageSrc": "imageSrc1"
+    },
+    {
+      "name": "name2",
+      "imageSrc": "imageSrc2"
+    },
+]
+  
+```
+method "name" is used to populate the list fields, and method "imageSrc" is used to open the image of the corresponding selected item.
+
 ## Usage
 1. Click on the "Open list" button.
-2. Select the desired car from the drop-down list.
-3. Clicking on a car will update the button text and display the image of the selected car.
+2. Select the desired it from the drop-down list.
+3. Clicking on a item will update the button text and display the image of the selected item.
 
 ## Features
-1. Drop-down list: User can open the list of cars by clicking on the "Open list" button.
-2. Car Selection: When you click on a car name from the list, it is set as the button text and an image of the selected car is displayed.
+1. Drop-down list: User can open the list of items by clicking on the "Open list" button.
+2. item Selection: When you click on a item name from the list, it is set as the button text and an image of the selected item is displayed.
 3. Interactivity: List items are enlarged on hover, improving user experience.
 4. Responsive Design: The app displays correctly on different devices thanks to responsive CSS.
 
@@ -32,4 +55,5 @@ Open index.html in a web browser: Find the index.html file in the project folder
 1. index.html: The main HTML page of the app.
 2. resources/styles.css: CSS file for styling the app.
 3. resources/script.js: JavaScript file for adding interactivity and app logic.
-4. resources/images/: Folder containing car images.
+4. resources/images/: Folder containing item images.
+5. resources/data/meta.json: data for filling lists.
--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,91 +1,109 @@
-window.onload = function() {
-const carsMeta = {
-    mazdaMiata: {
-        name: "Mazda Miata",
-        imageSrc: "mazdaMiata.jpg"
-    },
-    ferrariF40: {
-        name: "Ferrari F40",
-        imageSrc: "ferrariF40.jpg"
-    },
-    toyotaLevin: {
-        name: "Toyota Levin",
-        imageSrc: "toyotaLevin.jpg"
-    },
-    chevroletCorvette: {
-        name: "Chevrolet Corvette",
-        imageSrc: "corvet.jpg"
-    }  
-}
-
-const startButtons = document.querySelectorAll(".start-button")
-const dropDownListsSelectItem = document.querySelectorAll(".drop-down-list")
-
-function openImageSelectItem(currentList) {
-    currentList.parentElement.querySelector(".image-select-item").style.display = "block"
-}
-
-function closeimageSelectItem(currentList) {
-    currentList.querySelector(".image-select-item").style.display = "none"
-}
-
-function openDropdownList(event) {
-    event.stopPropagation();
-    const container = event.target.closest('.drop-down-list-container');
-    const dropDownList = container.querySelector('.drop-down-list');
-    dropDownList.style.display = "flex";
-    event.target.classList.remove("start-button-selected");
-    closeimageSelectItem(container); 
-}
-
-function closeDropDownAllLists() {
-    dropDownListsSelectItem.forEach(dropDownListSelectItem => {
-        dropDownListSelectItem.style.display = "none"
-    })
-}
-
-function closeDropDownList(currentList) {
-    currentList.style.display = "none"
-}
-
-function setUpImageSrc(selectedItemKey, currentImage) {
-    currentImage.src = "resources/images/cars/"+carsMeta[selectedItemKey].imageSrc
-    console.log("resources/images/cars/"+carsMeta[selectedItemKey].imageSrc)  
-}
-
-function handleClick(event) {
-    event.stopPropagation()
-    if (!event.target.closest(".drop-down-list") || !(event.target.className === "drop-down-list-item")) {
-        closeDropDownAllLists()
-        return
+window.onload = function () {
+  let itemsMeta;
+  const itemsMetaURL = "http://127.0.0.1:5500/resources/data/meta.json";
+  async function loadItemsMeta(itemsMetaURL) {
+    try {
+      const response = await fetch(itemsMetaURL);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      itemsMeta = await response.json();
+      initDropDownLists();
+    } catch (error) {
+      console.error("Ошибка при загрузке данных:", error.message);
     }
-    const currentDropDownList = event.target.closest(".drop-down-list")
-    const currentStartButton = currentDropDownList.parentElement.querySelector(".start-button")
-    const currentImageSelectItem = currentDropDownList.parentElement.querySelector(".image-select-item")
-    const selectItemName = event.target.textContent
-    currentStartButton.textContent = selectItemName
-    currentStartButton.classList.add("start-button-selected")
-    setUpImageSrc(event.target.getAttribute("data-item-key"), currentImageSelectItem)
-    openImageSelectItem(currentDropDownList)
-    closeDropDownList(currentDropDownList)
-}
+  }
 
-function createDropDownListsContent() {
-    dropDownListsSelectItem.forEach(dropDownListSelectCar => {
-        for (const item in carsMeta)
-            {
-                const itemSelectionButton = document.createElement('div')
-                itemSelectionButton.textContent = carsMeta[item].name
-                itemSelectionButton.classList.add("drop-down-list-item")
-                itemSelectionButton.setAttribute("data-item-key", item)
-                dropDownListSelectCar.appendChild(itemSelectionButton)
-            }
-    })
-}
+  const dropDownListContainers = document.querySelectorAll(
+    ".drop-down-list-container"
+  );
+  const dropDownLists = document.querySelectorAll(".drop-down-list");
 
-createDropDownListsContent()
-startButtons.forEach(startButton => {
-    startButton.addEventListener('click', openDropdownList)
-});
-document.addEventListener('click',handleClick)
-}
+  function setUpImageSrc(selectItemImage, selectItemName) {
+    const selectItemSrc = itemsMeta.find(
+      (unit) => unit.name === selectItemName
+    ).imageSrc;
+    selectItemImage.src = "resources/images/items/" + selectItemSrc;
+  }
+
+  function openImageSelectItem(selectItemImage) {
+    selectItemImage.style.display = "block";
+  }
+
+  function closeImageSelectItem(selectItemImage) {
+    selectItemImage.style.display = "none";
+  }
+
+  function openDropdownList(
+    startButton,
+    dropDownListSelectItem,
+    selectItemImage,
+    event
+  ) {
+    event.stopPropagation();
+    closeDropdownLists();
+    dropDownListSelectItem.style.display = "flex";
+    startButton.classList.remove("button-selected");
+    closeImageSelectItem(selectItemImage);
+  }
+
+  function closeDropdownLists() {
+    dropDownLists.forEach((dropDownList) => {
+      dropDownList.style.display = "none";
+    });
+  }
+
+  function clickNotOnLists(event) {
+    if (!(event.target.className === "drop-down-list-item")) {
+      closeDropdownLists();
+      return;
+    }
+  }
+
+  function selectProcess(startButton, selectItemImage, event) {
+    if (!event.target.closest(".drop-down-list-item")) {
+      return;
+    }
+    const selectItemName = event.target.closest(
+      ".drop-down-list-item"
+    ).textContent;
+    startButton.textContent = selectItemName;
+    startButton.classList.add("button-selected");
+    setUpImageSrc(selectItemImage, selectItemName);
+    openImageSelectItem(selectItemImage);
+    closeDropdownLists();
+  }
+
+  function initDropDownLists() {
+    dropDownListContainers.forEach((dropDownListContainer) => {
+      const startButton = dropDownListContainer.querySelector(".start-button");
+      const selectItemImage =
+        dropDownListContainer.querySelector(".image-select-item");
+      const dropDownListSelectItem =
+        dropDownListContainer.querySelector(".drop-down-list");
+      function createDropDownListContent() {
+        itemsMeta.forEach((item) => {
+          const itemSelectionButton = document.createElement("div");
+          itemSelectionButton.textContent = item.name;
+          itemSelectionButton.classList.add("drop-down-list-item");
+          dropDownListSelectItem.appendChild(itemSelectionButton);
+        });
+      }
+      createDropDownListContent();
+      dropDownListSelectItem.addEventListener("click", function () {
+        selectProcess(startButton, selectItemImage, event);
+      });
+      startButton.addEventListener("click", function () {
+        openDropdownList(
+          startButton,
+          dropDownListSelectItem,
+          selectItemImage,
+          event
+        );
+      });
+    });
+  }
+
+  loadItemsMeta(itemsMetaURL);
+  document.addEventListener("click", clickNotOnLists);
+};
